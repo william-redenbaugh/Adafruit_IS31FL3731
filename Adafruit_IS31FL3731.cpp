ebe099bf27c64fd9e80f5053265280fd38cba89a
ladyada <limor@ladyada.net>
2016-03-08 17:23:17 -0500

    init

---
+++ Adafruit_IS31FL3731.cpp
@@ -0,0 +1,117 @@
+#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Wire.h>
+#include <Adafruit_IS31FL3731.h>
+
+
+/* Constructor */
+Adafruit_IS31FL3731::Adafruit_IS31FL3731(void) : Adafruit_GFX(16, 9) {
+
+}
+
+boolean Adafruit_IS31FL3731::begin(uint8_t addr) {
+  Wire.begin();
+
+  _i2caddr = addr;
+  _frame = 0;
+
+  // shutdown
+  writeRegister8(ISSI_BANK_FUNCTIONREG, ISSI_REG_SHUTDOWN, 0x00);
+
+  delay(10);
+
+  // out of shutdown
+  writeRegister8(ISSI_BANK_FUNCTIONREG, ISSI_REG_SHUTDOWN, 0x01);
+
+  // picture mode
+  writeRegister8(ISSI_BANK_FUNCTIONREG, ISSI_REG_CONFIG, ISSI_REG_CONFIG_PICTUREMODE);
+
+  displayFrame(_frame);
+
+  // all LEDs on
+  for (uint8_t f=0; f<8; f++) {
+    for (uint8_t i=0; i<=0x11; i++)
+      writeRegister8(f, i, 0xff);   // frame 0, each 8 LEDs on
+
+    // & 0 PWM
+    for (uint8_t i=0; i<144; i++)
+      setLEDPWM(i, 0x0, f);           // set each led to 0 PWM, frame 0
+  }
+
+  audioSync(false);
+
+  return true;
+}
+
+
+void Adafruit_IS31FL3731::setLEDPWM(uint8_t lednum, uint8_t pwm, uint8_t bank) {
+  if (lednum >= 144) return;
+  writeRegister8(bank, 0x24+lednum, pwm);
+}
+
+
+void Adafruit_IS31FL3731::drawPixel(int16_t x, int16_t y, uint16_t color) {
+  if ((x < 0) || (x >= 16)) return;
+  if ((y < 0) || (y >= 9)) return;
+  if (color > 255) color = 255; // PWM 8bit max
+
+  setLEDPWM(x + y*16, color, _frame);
+  return;
+}
+
+void Adafruit_IS31FL3731::setFrame(uint8_t f) {
+  _frame = f;
+}
+
+void Adafruit_IS31FL3731::displayFrame(uint8_t f) {
+  if (f > 7) f = 0;
+  writeRegister8(ISSI_BANK_FUNCTIONREG, ISSI_REG_PICTUREFRAME, f);
+}
+
+
+void Adafruit_IS31FL3731::selectBank(uint8_t b) {
+ Wire.beginTransmission(_i2caddr);
+ Wire.write((byte)ISSI_COMMANDREGISTER);
+ Wire.write(b);
+ Wire.endTransmission();
+}
+
+void Adafruit_IS31FL3731::audioSync(boolean sync) {
+  if (sync) {
+    writeRegister8(ISSI_BANK_FUNCTIONREG, ISSI_REG_AUDIOSYNC, 0x1);
+  } else {
+    writeRegister8(ISSI_BANK_FUNCTIONREG, ISSI_REG_AUDIOSYNC, 0x0);
+  }
+}
+
+/*************/
+void Adafruit_IS31FL3731::writeRegister8(uint8_t b, uint8_t reg, uint8_t data) {
+  selectBank(b);
+
+  Wire.beginTransmission(_i2caddr);
+  Wire.write((byte)reg);
+  Wire.write((byte)data);
+  Wire.endTransmission();
+  //Serial.print("$"); Serial.print(reg, HEX);
+  //Serial.print(" = 0x"); Serial.println(data, HEX);
+}
+
+uint8_t  Adafruit_IS31FL3731::readRegister8(uint8_t bank, uint8_t reg) {
+ uint8_t x;
+
+ selectBank(bank);
+
+ Wire.beginTransmission(_i2caddr);
+ Wire.write((byte)reg);
+ Wire.endTransmission();
+
+ Wire.beginTransmission(_i2caddr);
+ Wire.requestFrom(_i2caddr, (byte)1);
+ x = Wire.read();
+ Wire.endTransmission();
+
+// Serial.print("$"); Serial.print(reg, HEX);
+//  Serial.print(": 0x"); Serial.println(x, HEX);
+
+  return x;
+}
