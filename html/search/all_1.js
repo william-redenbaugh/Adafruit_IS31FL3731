b823448a24572f9ea469269b55e11067a18b0d8c
Doxygen CI <travis@travis-ci.org>
2020-02-07 20:50:28 +0000

    Deploy code docs to GitHub Pages Travis build: 13
    Commit: 15ac65447c4981f18b5a236f9a3b14fab7dc303a

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['adafruit_5fis31fl3731',['Adafruit_IS31FL3731',['../class_adafruit___i_s31_f_l3731.html',1,'Adafruit_IS31FL3731'],['../class_adafruit___i_s31_f_l3731.html#a44c584ec65d060ccc8d6282ed26f248e',1,'Adafruit_IS31FL3731::Adafruit_IS31FL3731()']]],
   ['adafruit_5fis31fl3731_5fwing',['Adafruit_IS31FL3731_Wing',['../class_adafruit___i_s31_f_l3731___wing.html',1,'Adafruit_IS31FL3731_Wing'],['../class_adafruit___i_s31_f_l3731___wing.html#a66328789fb70ce30627ce796a070b256',1,'Adafruit_IS31FL3731_Wing::Adafruit_IS31FL3731_Wing()']]],
-  ['audiosync',['audioSync',['../class_adafruit___i_s31_f_l3731.html#a80acec24f1c83e062123b4cbc0ffd538',1,'Adafruit_IS31FL3731']]]
+  ['audiosync',['audioSync',['../class_adafruit___i_s31_f_l3731.html#a77a0fe44d3d3de1ae58757205b3790b8',1,'Adafruit_IS31FL3731']]]
 ];
